b6805848d985de3c85a17d47dc9efe380b33f889
Oliver Wethey <oliver.musgrove-wethey@mayden.co.uk>
2015-10-14 17:05:49 +0100

    Add a label to the text fields

--- spec/App/ViewHelpers/InputTextSpec.php
+++ spec/App/ViewHelpers/InputTextSpec.php
@@ -14,26 +14,26 @@ class InputTextSpec extends ObjectBehavior
 
     public function it_should_return_an_input_field()
     {
-        $this->display([])->shouldReturn('<input type="text" name="" id="" class="" value="" />');
+        $this->display([])->shouldReturn('<label for=""></label><input type="text" name="" id="" class="" value="" />');
     }
 
     public function it_should_return_an_input_field_with_name()
     {
-        $this->display(['name' => 'name'])->shouldReturn('<input type="text" name="name" id="" class="" value="" />');
+        $this->display(['name' => 'name'])->shouldReturn('<label for="">name</label><input type="text" name="name" id="" class="" value="" />');
     }
 
     public function it_should_return_an_input_field_with_id()
     {
-        $this->display(['id' => 'id'])->shouldReturn('<input type="text" name="" id="id" class="" value="" />');
+        $this->display(['id' => 'id'])->shouldReturn('<label for="id"></label><input type="text" name="" id="id" class="" value="" />');
     }
 
     public function it_should_return_an_input_field_with_class()
     {
-        $this->display(['class' => 'class'])->shouldReturn('<input type="text" name="" id="" class="class" value="" />');
+        $this->display(['class' => 'class'])->shouldReturn('<label for=""></label><input type="text" name="" id="" class="class" value="" />');
     }
 
     public function it_should_return_an_input_field_with_value()
     {
-        $this->display(['value' => 'value'])->shouldReturn('<input type="text" name="" id="" class="" value="value" />');
+        $this->display(['value' => 'value'])->shouldReturn('<label for=""></label><input type="text" name="" id="" class="" value="value" />');
     }
 }
--- src/App/ViewHelpers/InputText.php
+++ src/App/ViewHelpers/InputText.php
@@ -9,7 +9,9 @@ class InputText implements ViewHelper
     public function display(array $properties)
     {
         return sprintf(
-            '<input type="text" name="%s" id="%s" class="%s" value="%s" />',
+            '<label for="%s">%s</label><input type="text" name="%s" id="%s" class="%s" value="%s" />',
+            isset($properties['id']) ? $properties['id'] : '',
+            isset($properties['name']) ? $properties['name'] : '',
             isset($properties['name']) ? $properties['name'] : '',
             isset($properties['id']) ? $properties['id'] : '',
             isset($properties['class']) ? $properties['class'] : '',
